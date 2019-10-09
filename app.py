b6bb2b49292b2a72acf0c01a780c6c6b37ccc8a7
Sebastian Flores <sebastiandres@Sebastians-MacBook-Pro.local>
2019-10-08 21:34:26 -0300

    Connecting to the database

--- app.py
+++ app.py
@@ -2,20 +2,28 @@ from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 import yaml
 
+# Parameters
+config_file = 'local.yml'
+
+# Initialize
 app = Flask(__name__)
 
 # Read the config properties
-config_file = 'local.yml'
 config_dict = yaml.load(open(config_file,"r"), Loader=yaml.FullLoader)
 app.config['MYSQL_HOST'] = config_dict["MYSQL_HOST"]
-app.config['MYSQL_USER'] = config_dict["MYSQL_HOST"]
-app.config['MYSQL_PASSWORD'] = config_dict["MYSQL_HOST"]
-app.config['MYSQL_DB'] = config_dict["MYSQL_HOST"]
-debug_mode = bool(config_dict["DEBUG_MODE"])
+app.config['MYSQL_USER'] = config_dict["MYSQL_USER"]
+app.config['MYSQL_PASSWORD'] = config_dict["MYSQL_PASSWORD"]
+app.config['MYSQL_DB'] = config_dict["MYSQL_DB"]
+DEBUG_MODE = bool(config_dict["DEBUG_MODE"])
 
+# Apply the configuration
 mysql = MySQL(app)
+# Test the connection
+print("Must test the connection")
 
-@app.route('/', methods=['GET', 'POST'])
+DEBUG_MODE = True
+
+@app.route('/')
 def index():
     if request.method == "POST":
         details = request.form
@@ -46,7 +54,19 @@ def line_chart():
 
 @app.route('/bar_chart/', methods=['GET'])
 def bar_chart():
-    return render_template('bar_chart.html')
+    survey_answers = {"Pregunta 1":10, "Pregunta 2":20, "Pregunta 3":30}
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT question_str FROM surveyQuestions WHERE survey_id=1")
+    mysql_data = cur.fetchall()
+    print(type(mysql_data), mysql_data)
+    title  = mysql_data[0][0]
+    chart_data = {}
+    chart_data["title"] = title
+    chart_data["xLabel"] = "Opciones"
+    chart_data["yLabel"] = "Número de Respuestas"
+    chart_data["data.labels"] = ["Opcion 1", "Opcion 2", "Opcion 3"]
+    chart_data["data.datasets.data"] = [2, 4, 6]
+    return render_template('bar_chart.html', data=chart_data)
 
 @app.route('/xy_chart/', methods=['GET'])
 def xy_chart():
@@ -57,4 +77,4 @@ def radar_chart():
     return render_template('radar_chart.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=DEBUG_MODE)
